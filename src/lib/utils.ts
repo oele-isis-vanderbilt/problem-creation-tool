ce5cc9912224171f2e058a2d475c745877d8c332
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2025-05-05 18:09:57 -0500

    Add debounce function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,17 @@ import type { AgentAuth } from '@knowlearning/agents/browser';
 export const isAnonUser = (auth: AgentAuth) => {
 	return auth.info?.name === 'anonymous';
 };
+
+export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
+	func: F,
+	waitFor: number
+) => {
+	let timeout: NodeJS.Timeout;
+
+	const debounced = (...args: Parameters<F>) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => func(...args), waitFor);
+	};
+
+	return debounced;
+};
