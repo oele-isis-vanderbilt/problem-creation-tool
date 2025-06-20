449de71b1640c8a04a040f090f54beef3e98378d
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2025-06-20 17:34:44 -0500

    Only initialize stores during (app) route group. Closes #92 (#93)
    * Only initialize stores during (app) route group. Closes #92
    
    * Add formatting fix

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -3,7 +3,10 @@ export const ssr = false;
 export const prerender = false;
 
 export const load: LayoutLoad = async () => {
-	const { getEnvironment } = await import('$lib/services/knowLearningStore.svelte');
+	const { initialize, getEnvironment } = await import('$lib/services/knowLearningStore.svelte');
+	const { initProblemStore } = await import('$lib/services/problem-store.svelte');
+	const problemStore = await initProblemStore();
+	await initialize(problemStore);
 	const env = await getEnvironment();
 	return {
 		env
